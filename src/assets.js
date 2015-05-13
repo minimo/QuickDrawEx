eaf358240c97538f4d3f8f5a65a27f8df446e02f
minimo <minimo.mobile@gmail.com>
2015-05-13 12:11:16 +0900

    Initial files

---
+++ src/assets.js
@@ -0,0 +1,13 @@
+/*
+ *  assets.js
+ *  2014/06/04
+ *  @auther minimo  
+ *  This Program is MIT license.
+ */
+
+var assets = {
+    //Font
+    "Yasashisa":    "font/07YasashisaAntique.ttf",
+    "CasinoRegular":"font/CasinoRegular.ttf",
+    "UbuntuMono":   "font/UbuntuMono-Bold.ttf",
+}
