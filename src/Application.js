fa03b8dcfd724b32f55dbcf0aec7e679a6404c64
minimo <minimo.mobile@gmail.com>
2015-05-14 15:45:19 +0900

    fix

--- src/Application.js
+++ src/Application.js
@@ -40,7 +40,7 @@ tm.define("tmapp.CanvasApp", {
 
         //アセット読み込み
         var loadingScene = tmapp.LoadingScene({
-            assets: tmapp.assets["main"],
+            assets: assets["main"],
             width: SC_W,
             height: SC_H,
             bgColor: 'rgba(0, 0, 0, 1)',
