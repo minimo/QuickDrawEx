3326bf054aafb24669b5455926bf7ad701fa9e14
minimo <minimo.mobile@gmail.com>
2015-06-04 11:55:23 +0900

    add assets

--- src/assets.js
+++ src/assets.js
@@ -10,7 +10,12 @@ assets = [];
 assets["main"] = {
     //Sprite
     "hiyoko":   "assets/hiyoco_nomal_full.png",
+
+    //Sound
+    "bang":     "assets/se_bang.mp3",
+
     //Font
     "UbuntuMono":   "font/UbuntuMono-Bold.ttf",
     "Orbitron":     "font/Orbitron-Regular.ttf",
+    "Digital":      "font/Let's go Digital Regular.ttf",
 }
